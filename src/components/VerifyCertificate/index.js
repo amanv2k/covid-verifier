04b64829d04227af2960757326db8c2262a4f5f5
amanv2k <amanvermaav2605@gmail.com>
2022-03-31 01:13:48 +0530

    vaccine verification part

---
+++ src/components/VerifyCertificate/index.js
@@ -0,0 +1,73 @@
+import React, {useState} from "react";
+import "./index.css";
+import VerifyCertificateImg from "../../assets/img/verify-certificate.png"
+import QRCodeImg from "../../assets/img/qr-code.svg"
+import {CertificateStatus} from "../CertificateStatus";
+import {CustomButton} from "../CustomButton";
+import QRScanner from "../QRScanner";
+import JSZip from "jszip";
+import Container from "react-bootstrap/Container";
+export const CERTIFICATE_FILE = "certificate.json";
+
+export const VerifyCertificate = () => {
+    const [result, setResult] = useState("");
+    const [showScanner, setShowScanner] = useState(false);
+    const handleScan = data => {
+        if (data) {
+            const zip = new JSZip();
+            zip.loadAsync(data).then((contents) => {
+                return contents.files[CERTIFICATE_FILE].async('text')
+            }).then(function (contents) {
+                setResult(contents)
+            }).catch(err => {
+                    setResult(data)
+                }
+            );
+
+        }
+    };
+    const handleError = err => {
+        console.error(err)
+    };
+    return (
+        <div className="container-fluid verify-certificate-wrapper">
+            {
+                !result &&
+                <>
+                    {!showScanner &&
+                    <>
+                        <img src={VerifyCertificateImg} className="banner-img" alt="banner-img"/>
+                        <h3 className="text-center">Verify a vaccination certificate</h3>
+                        <CustomButton className="green-btn" onClick={() => setShowScanner(true)}>
+                            <span>Scan QR code</span>
+                            <img className="ml-3" src={QRCodeImg} alt={""}/>
+                        </CustomButton>
+                        <Container className="mt-5 p-4 mb-5">
+                            <p>
+                                The Co-WIN vaccination certificate has a digitally signed secure QR code.
+
+                                This can be authenticated online  using the verification utility in this portal or using third party verification app using the services outlined below.
+                            </p>
+                        </Container>
+                    </>}
+                    {showScanner &&
+                    <>
+                        <QRScanner onError={handleError}
+                                   onScan={handleScan}/>
+                        <CustomButton className="green-btn" onClick={() => setShowScanner(false)}>BACK</CustomButton>
+                    </>
+                    }
+                </>
+            }
+            {
+                result && <CertificateStatus certificateData={result} goBack={() => {
+                    setShowScanner(false);
+                    setResult("");
+                }
+                }/>
+            }
+
+
+        </div>
+    )
+};
