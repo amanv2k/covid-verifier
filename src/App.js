d04d280be9607f0866c5898334b1a438c4814a8e
amanv2k <amanvermaav2605@gmail.com>
2022-04-09 16:38:20 +0530

    added barcode reader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {VerifyCertificate} from "./components/VerifyCertificate";
+import { BarcodeScanner } from "./components/BarcodeScanner";
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
 
@@ -7,7 +8,8 @@ function App() {
   return (
       <Provider store={store}>
         <div className="App">
-            <VerifyCertificate />
+          <VerifyCertificate />
+            {/* <BarcodeScanner /> */}
         </div>
       </Provider>
   );
