b8955c9ee402805a3eae4e2b154125a1015ebb2f
amanv2k <amanvermaav2605@gmail.com>
2022-04-10 20:51:02 +0530

    added basic main page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+import React from "react";
 import './App.css';
-import {VerifyCertificate} from "./components/VerifyCertificate";
-import { BarcodeScanner } from "./components/BarcodeScanner";
-import {Provider} from "react-redux";
-import {store} from "./redux/store";
+import { VerifyCertificate } from "./components/VerifyCertificate";
+// import { BarcodeScanner } from "./components/BarcodeScanner";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 function App() {
+  const [app, setApp] = React.useState(null);
+
+  const getPage = () => {
+    switch (app) {
+      case 0:
+        return <VerifyCertificate />
+      case 1:
+        return <></>
+      case null:
+      default:
+        return <div>Please select the app.</div>
+    }
+  }
+
   return (
-      <Provider store={store}>
-        <div className="App">
-          <VerifyCertificate />
-            {/* <BarcodeScanner /> */}
-        </div>
-      </Provider>
+    <Provider store={store}>
+      <button onClick={() => setApp(0)}>Verify Certificate</button>
+      <button onClick={() => setApp(1)}>Barcode Scanner</button>
+      <div className="App">
+        {getPage()}
+        {/* <BarcodeScanner /> */}
+      </div>
+    </Provider>
   );
 }
 
