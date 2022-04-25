4ca5e20e6b34d22668c775b0dbd3c9d115ed7253
amanv2k <amanvermaav2605@gmail.com>
2022-04-25 22:12:56 +0530

    changes in barcode reader system

--- src/components/BarcodeReader/index.js
+++ src/components/BarcodeReader/index.js
@@ -1,25 +1,46 @@
 import React, { useEffect } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
-export const BarcodeReader=()=> {
+export const BarcodeReader = () => {
   const [data, setData] = React.useState([]);
+  const [validBarcodes, setValidBarcodes] = React.useState([]);
+  const [isValid, setIsValid] = React.useState(null);
 
   const update = React.useCallback((err, result) => {
-    if (result && !data.includes(result)) setData([...data, result.text]);
-  }, data);
+    if (result && !data.includes(result)) {
+      setData([...data, result.text]);
+    }
+  }, [data, setData]);
 
-  useEffect(() => {
-    console.log(data)
-  }, [data])
+  React.useEffect(() => {
+    setIsValid(validBarcodes.includes(data[data.length - 1]));
+  }, [validBarcodes, setIsValid, data]);
+
+  const handleFile = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const data = e.target.result;
+      const lines = data.split("\n").map(line => parseInt(line).toString());
+      setValidBarcodes([...lines]);
+    }
+
+    reader.readAsText(file);
+  };
 
   return (
     <>
+      <div>
+        <input type="file" onChange={handleFile} />
+      </div>
       <BarcodeScannerComponent
         width={500}
         height={500}
         onUpdate={update}
       />
       <p>{data}</p>
+      {isValid !== null ? <p>{isValid ? "Valid" : "Invalid"}</p> : ""}
     </>
   );
 }
